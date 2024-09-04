14799906edeb233ca9eb0ca05d496c4f87d845c6
Samuel Quintana <sock.sqt@gmail.com>
2024-09-04 11:26:30 -0600

    feat: Created project base

---
+++ backend/src/main/java/com/banorte/account/transactions/AccountTransactionsApplication.java
@@ -0,0 +1,12 @@
+package com.banorte.account.transactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountTransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountTransactionsApplication.class, args);
+	}
+}
